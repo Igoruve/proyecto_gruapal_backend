368535ff7d8dd9742118197d093d328d2e678dca
Kai <jauregiazcue@gmail.com>
2025-04-14 19:18:27 +0200

    change product add redirection into login

--- src/middlewares/orderProductMiddleware.js
+++ src/middlewares/orderProductMiddleware.js
@@ -11,25 +11,25 @@ async function addToOrder(req, res) {
     res.redirect("/products");
   } catch (error) {
     console.error(error);
-    res.redirect("/products");
+    res.redirect("/login");
   }
 }
 
 async function removeFromOrder(req, res) {
-    try {
-      const id = req.params.id;
-      const userId = req.session.user.user_id;
-      const order = await orderController.controllerGetByUserID(userId);
-      const product = await productController.controllerGetByID(id);
-      order.removeProduct(product);
-      res.redirect("/orders");
-    } catch (error) {
-      console.error(error);
-      res.render("product", { error: "Internal server error" });
-    }
+  try {
+    const id = req.params.id;
+    const userId = req.session.user.user_id;
+    const order = await orderController.controllerGetByUserID(userId);
+    const product = await productController.controllerGetByID(id);
+    order.removeProduct(product);
+    res.redirect("/orders");
+  } catch (error) {
+    console.error(error);
+    res.render("product", { error: "Internal server error" });
   }
+}
 
 export default {
-    addToOrder,
-    removeFromOrder
+  addToOrder,
+  removeFromOrder
 };
